b35fff79f552381f559fb0497d770d3d3b030c4b
sdegrijff <sander@degrijff.com>
2025-12-30 20:46:55 +0100

    Create functions.php

---
+++ themes/ggr-portal/assets/css/functions.php
@@ -0,0 +1,11 @@
+body.page-wijziging .ggr-shell-main,
+body.page-verwijs-een-vriend .ggr-shell-main {
+    display: none;
+}
+
+.ggr-shell-nav a[href*="/wijziging/"],
+.ggr-mobile-footer a[href*="/wijziging/"],
+.ggr-shell-nav a[href*="/verwijs-een-vriend/"],
+.ggr-mobile-footer a[href*="/verwijs-een-vriend/"] {
+    display: none;
+}
